a81435a4a8cf4aea0ee209ac66b4ad3b88fcb4dd
Fran Uncal <franuncal@gmail.com>
2025-01-01 20:43:54 -0300

    Agregando footer dinamico

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,10 @@ export const Footer = () => {
     <footer className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-left">
-          <p>&copy; 2024 FranUncal || Todos los derechos reservados.</p>
+          <p>
+            © {new Date().getFullYear()} Fran Uncal | Todos los derechos
+            reservados.
+          </p>
         </div>
       </div>
     </footer>
